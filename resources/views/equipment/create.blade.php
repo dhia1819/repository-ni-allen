55551f3230b8e040eb435b031e99d1f5e6be0aac
dhia1819 <dhiaaguila@gmail.com>
2024-04-02 10:38:03 +0800

    unit of measure and condition added

--- resources/views/equipment/create.blade.php
+++ resources/views/equipment/create.blade.php
@@ -28,48 +28,65 @@
                 <form method="POST" action="{{ route('equipment.store') }}" enctype="multipart/form-data" id="addEquipment">
                     @csrf
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-4">
+                            <label class="form-label" for="date_acquired">Date Acquired<span class="text-danger">&#x2022;</span></label>
+                            <input type="date" class="form-control" id="date_acquired" name="date_acquired">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label" for="property_no">Property Number<span class="text-danger">&#x2022;</span></label>
+                            <input type="text" class="form-control" id="property_no" name="property_no">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label" for="serial_no">Serial Number<span class="text-danger">&#x2022;</span></label>
+                            <input type="text" class="form-control" id="serial_no" name="serial_no">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-4">
                             <label class="form-label" for="equipment_name">Equipment Name <span class="text-danger">&#x2022;</span></label>
                             <input type="text" class="form-control" id="equipment_name" name="equipment_name">
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <label class="form-label" for="category">Category<span class="text-danger">&#x2022;</span></label>
                             <select class="form-control select select2-create" id="category" name="category">
-                                <option value="">Select Category</option>
+                                <option selected disabled>Select Category</option>
                                 @foreach($categories as $category)
                                     <option value="{{ $category->id }}">{{ $category->category }}</option>
                                 @endforeach
                             </select>
                         </div>
+                        <div class="col-md-4">
+                            <label class="form-label" for="conditions">Conditions<span class="text-danger">&#x2022;</span></label>
+                            <select class="form-control select select2-create" id="conditions" name="conditions">
+                                <option selected disabled>Select Condition</option>
+                                <option value="Good" >Good</option>
+                                <option value="Fair">Fair</option>
+                                <option value="Poor">Poor</option>
+                                <option value="Damaged">Damaged</option>
+                                <option value="Obsolete">Obsolete</option>
+                                <option value="Unusable">Unusable</option>
+                                <option value="Under Maintenance">Under Maintenance</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6">
-                            <label class="form-label" for="description">Description<span class="text-danger">&#x2022;</span></label>
-                            <textarea class="form-control" id="Description" name="Description"></textarea>
+                        <div class="col-md-4">
+                            <label class="form-label" for="unit_of_measure">Unit of Measure<span class="text-danger">&#x2022;</span></label>
+                            <input type="text" class="form-control" id="unit_of_measure" name="unit_of_measure">
                         </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="property_no">Property Number<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="property_no" name="property_no">
+                        <div class="col-md-4">
+                            <label class="form-label" for="quantity">Quantity<span class="text-danger">&#x2022;</span></label>
+                            <input type="text" class="form-control" id="quantity" name="quantity">
                         </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6">
-                            <label class="form-label" for="serial_no">Serial Number<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="serial_no" name="serial_no">
-                        </div>
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <label class="form-label" for="value">Value<span class="text-danger">&#x2022;</span></label>
                             <input type="text" class="form-control" id="value" name="value">
                         </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6">
-                            <label class="form-label" for="quantity">Quantity<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="quantity" name="quantity">
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="image">Image <span class="text-info">(Optional)</span></label>
-                            <input type="file" class="form-control" id="image" name="image">
+                        <div class="col-md-12">
+                            <label class="form-label" for="description">Description<span class="text-danger">&#x2022;</span></label>
+                            <textarea class="form-control" id="Description" name="Description"></textarea>
                         </div>
                     </div>
                     <div class="row mt-3">
@@ -78,8 +95,8 @@
                             <input type="text" class="form-control" id="remarks" name="remarks">
                         </div>
                         <div class="col-md-6">
-                            <label class="form-label" for="date_acquired">Date Acquired<span class="text-danger">&#x2022;</span></label>
-                            <input type="date" class="form-control" id="date_acquired" name="date_acquired">
+                            <label class="form-label" for="image">Image <span class="text-info">(Optional)</span></label>
+                            <input type="file" class="form-control" id="image" name="image">
                         </div>
                     </div>
                     <div class="row mt-3">
--- resources/views/equipment/edit.blade.php
+++ resources/views/equipment/edit.blade.php
@@ -33,12 +33,12 @@
                     <div class="row">
                         <div class="col-md-4">
                             <div class="row">
-                                <div class="row">
+                                <div class="col-md-12">
                                     <label class="form-label" for="image">Image<span class="text-danger">&#x2022;</span></label>
                                     <input type="file" class="form-control" id="image" name="image">
                                     
                                 </div>
-                                <div class="row">
+                                <div class="col-md-12">
                                     <div class="image-preview-container text-center mt-2">
                                         <img id="image-preview" src="{{ $equipment->image ? asset('uploads/' . $equipment->image) : 'https://via.placeholder.com/100x100' }}" alt="Image Preview" class="img-fluid" style="width: 300px; height: 300px;">
                                     </div>
@@ -46,78 +46,59 @@
                             </div>
                             
                         </div>
-<<<<<<< HEAD
-                        <div class="col-md-4">
-                            <label class="form-label" for="equipment_name">Equipment Name <span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="equipment_name" name="equipment_name" value="{{ $equipment->equipment_name }}">
-                        </div>
-                        <div class="col-md-4">
-                            <label class="form-label" for="category">Category<span class="text-danger">&#x2022;</span></label>
-                            <select class="form-control select select2-update" id="category" name="category">
-                                <option value="">Select Category</option>
-                                @foreach($categories as $category)
-                                @if($category->status == 0)
-                                    @continue
-                                @endif
-                                <option value="{{ $category->id }}" {{ $category->id == $equipment->category ? 'selected' : '' }}>{{ $category->category }}</option>
-                            @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-4">
-                            <label class="form-label" for="property_no">Property Number<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="property_no" name="property_no" value="{{ $equipment->property_no }}">
-                        </div>
-                        <div class="col-md-4">
-                            <label class="form-label" for="serial_no">Serial Number<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="serial_no" name="serial_no" value="{{ $equipment->serial_no }}">
-                        </div>
-                        <div class="col-md-4">
-                            <label class="form-label" for="value">Value<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="value" name="value" value="{{ $equipment->value }}">
-                        </div>
-                                               
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6">
-                            <label class="form-label" for="quantity">Quantity<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="quantity" name="quantity" value="{{ $equipment->quantity }}">
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="remarks">Remarks<span class="text-danger">&#x2022;</span></label>
-                            <input type="text" class="form-control" id="remarks" name="remarks" value="{{ $equipment->remarks }}">
-=======
                         <div class="col-md-8">
                             <div class="row">
-                                <div class="col-md-6">
-                                    <label class="form-label" for="equipment_name">Equipment Name <span class="text-danger">&#x2022;</span></label>
-                                    <input type="text" class="form-control" id="equipment_name" name="equipment_name" value="{{ $equipment->equipment_name }}">
-                                </div>
-                                <div class="col-md-6">
-                                    <label class="form-label" for="category">Category<span class="text-danger">&#x2022;</span></label>
-                                    <select class="form-control select select2-update" id="category" name="category">
-                                        <option value="">Select Category</option>
-                                        @foreach($categories as $category)
-                                            <option value="{{ $category->id }}" {{ $category->id == $equipment->category ? 'selected' : '' }}>{{ $category->category }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
                                 <div class="col-md-6">
                                     <label class="form-label" for="date_acquired">Date Acquired<span class="text-danger">&#x2022;</span></label>
                                     <input type="date" class="form-control" id="date_acquired" name="date_acquired" value="{{ $equipment->date_acquired }}">
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="form-label" for="property_no">Property Number<span class="text-danger">&#x2022;</span></label>
-                                    <input type="text" class="form-control" id="property_no" name="property_no" value="{{ $equipment->property_no }}">
+                                    <label class="form-label" for="equipment_name">Equipment Name <span class="text-danger">&#x2022;</span></label>
+                                    <input type="text" class="form-control" id="equipment_name" name="equipment_name" value="{{ $equipment->equipment_name }}">
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-md-6">
+                                    <label class="form-label" for="category">Category<span class="text-danger">&#x2022;</span></label>
+                                    <select class="form-control select select2-update" id="category" name="category">
+                                        <option value="">Select Category</option>
+                                        @foreach($categories as $category)
+                                            @if($category->status ==0)
+                                                @continue
+                                            @endif
+                                            <option value="{{ $category->id }}" {{ $category->id == $equipment->category ? 'selected' : '' }}>{{ $category->category }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="form-label" for="conditions">Condition<span class="text-danger">&#x2022;</span></label>
+                                    <select class="form-control select select2-update" id="conditions" name="conditions">
+                                        <option selected disabled>{{ $equipment->conditions }}</option>
+                                        <option value="Good" >Good</option>
+                                        <option value="Fair">Fair</option>
+                                        <option value="Poor">Poor</option>
+                                        <option value="Damaged">Damaged</option>
+                                        <option value="Obsolete">Obsolete</option>
+                                        <option value="Unusable">Unusable</option>
+                                        <option value="Under Maintenance">Under Maintenance</option>
+                                    </select>
+                                </div>
+                                
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6">
+                                    <label class="form-label" for="property_no">Property Number<span class="text-danger">&#x2022;</span></label>
+                                    <input type="text" class="form-control" id="property_no" name="property_no" value="{{ $equipment->property_no }}">
+                                </div>
+                                <div class="col-md-6">
                                     <label class="form-label" for="serial_no">Serial Number<span class="text-danger">&#x2022;</span></label>
                                     <input type="text" class="form-control" id="serial_no" name="serial_no" value="{{ $equipment->serial_no }}">
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6">
+                                    <label class="form-label" for="quantity">Quantity<span class="text-danger">&#x2022;</span></label>
+                                    <input type="text" class="form-control" id="quantity" name="quantity" value="{{ $equipment->quantity }}">
                                 </div>
                                 <div class="col-md-6">
                                     <label class="form-label" for="value">Value<span class="text-danger">&#x2022;</span></label>
@@ -126,9 +107,9 @@
                             </div>
                             <div class="row mt-3">
                                 <div class="col-md-6">
-                                    <label class="form-label" for="quantity">Quantity<span class="text-danger">&#x2022;</span></label>
-                                    <input type="text" class="form-control" id="quantity" name="quantity" value="{{ $equipment->quantity }}">
-                                </div>
+                                    <label class="form-label" for="unit_of_measure">Unit of Measure<span class="text-danger">&#x2022;</span></label>
+                                    <input type="text" class="form-control" id="unit_of_measure" name="unit_of_measure" value="{{ $equipment->unit_of_measure }}">
+                                </div>                                
                                 <div class="col-md-6">
                                     <label class="form-label" for="remarks">Remarks<span class="text-danger">&#x2022;</span></label>
                                     <input type="text" class="form-control" id="remarks" name="remarks" value="{{ $equipment->remarks }}">
@@ -146,7 +127,6 @@
                                        <i class="fa fa-paper-plane"></i> Submit</button>
                                 </div>
                             </div>
->>>>>>> a8ca49455eaf5284632279686b15c0a040299116
                         </div>
                         
                     </div>
