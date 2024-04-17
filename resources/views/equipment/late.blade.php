4e9b3345066da5471b7f6afff918f07cc7deb66b
maybeRaiven <raivenmuega@gmail.com>
2024-04-17 16:26:05 +0800

    filters for late table and borrowed table

--- resources/views/equipment/late.blade.php
+++ resources/views/equipment/late.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.master')
 @section('page_name', $page['name'])
 @section('page_script')
-    <script type="text/javascript" src="/js/borrow.js"></script>
+    <script type="text/javascript" src="/js/late.js"></script>
+    
 @endsection
 @section('page_css')
     <style type="text/css">
@@ -10,17 +11,17 @@
         /* Media query for small screens */
         @media (max-width: 768px) {
             /* Hide table headers */
-            #tbl-equipment th {
+            #tbl-late th {
                 display: none;
             }
 
             /* Show table data as block elements */
-            #tbl-equipment td {
+            #tbl-late td {
                 display: block;
             }
 
             /* Center-align table data */
-            #tbl-equipment td {
+            #tbl-late td {
                 text-align: center;
             }
 
@@ -36,48 +37,105 @@
         <div class="col-md-12">
             @include('layouts.message')
         </div>
-        <div class="col-12">
-            <!-- Category Filter -->
-            <div class="form-group">
-                <label for="category_filter">Filter by Category:</label>
-                <select class="form-control" id="category_filter">
-                    <option value="">All Categories</option>
-                    @foreach($categories as $category)
-                    @if($category->status == 0)
-                        @continue
-                    @endif
-                        <option value="{{ $category->category }}">{{ $category->category }}</option>
-                    @endforeach
-                </select>
+    </div>
+        
+    <div class="col-md-12">
+        <form action="{{ route('download.history') }}" method="GET">
+            @csrf
+            <div class="row">
+            <div class="col-md-12">
+               
+                    <a id="reset_filter" class="btn bg-gradient-danger " style="display:none;" >
+                        <i class="fa fa-undo mx-1"></i>Clear filters
+                    </a>
+             
+                <button type="submit" class="btn bg-gradient-success float-end">
+                    <i class="fa fa-download mx-1"></i> 
+                </button>  
             </div>
+            </div>
+            <div class="row">
+                <div class="col-12" id="filter">
+                    <div class="row col-12">
+                        <div class="form-group col-md-3">
+                            <label for="office_filter">Filter by Office:</label>
+                            <select class="form-control select2 select2-filter" id="office_filter" name="office_filter">
+                                <option value="">All Offices</option>
+                                @foreach($offices as $office)
+                                    <option value="{{ $office->code }}">{{ $office->code }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-3">
+                            <label for="employee_filter">Filter by Employee:</label>
+                            <select class="form-control select2 select2-filter" id="employee_filter" name="employee_filter">
+                                <option value="">All Employee</option>
+                                @foreach($employees as $employee)
+                                    <option value="{{ $employee->fullName }}">{{ $employee->fullName }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                       
+                        <div class="date-range-container col-md-3">
+                            <label for="start_date_borrowed" id="start_date_label">Date Borrowed | Start Date</label>
+                            <label for="end_date_borrowed" id="end_date_label" style="display: none;">Date Borrowed | End Date</label>
+                            <div class="date-inputs">
+                                <input type="date" class="form-control" id="start_date_borrowed" name="start_date_borrowed" onchange="handleStartDateChange('borrowed')">
+                                <input type="date" class="form-control" id="end_date_borrowed" name="end_date_borrowed" style="display: none;">
+                            </div>
+                        </div>
+                        
+                        <div class="date-range-container col-md-3">
+                            <label for="start_date_expect" id="start_date_expect_label">Expected Return | Start Date</label>
+                            <label for="end_date_expect" id="end_date_expect_label" style="display: none;">Expected Return | End Date</label>
+                            <div class="date-inputs">
+                                <input type="date" class="form-control" id="start_date_expect" name="start_date_expect" onchange="handleStartDateChange('returned')">
+                                <input type="date" class="form-control" id="end_date_expect" name="end_date_expect" style="display: none;">
+                            </div>
+                        </div>
+        
+                        
+                    </div>
+                </div>
+            </div>
+                
+                
+            </form>
+            </div>
+        
+    </div>
+
             <div class="col-12">
                 <div class="card mb-2">
                     <div class="card-header pb-0">
                         <h6>Missing Items</h6>
                     </div>
                     <div class="card-body">
-                        <table class="table align-items-center mb-0" id="tbl-equipment" style="width: 100%;">
+                        <table class="table align-items-center mb-0" id="tbl-late" style="width: 100%;">
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Borrower</th> 
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Office</th>
+                                    <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Released by:</th> 
 
                                     {{-- <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Equipment</th> 
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Property No#</th> 
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Serial No#</th>  --}}
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Date Borrowed</th> 
                                     <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Expected Return Date</th>
-                                    <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Released by:</th> 
+                                    
                                     {{-- <th class="text-uppercase text-dark text-xxs font-weight-bolder ps-2">Status</th>  --}}
                                     <th class="text-center text-uppercase text-dark text-xxs font-weight-bolder" width="11%">Status</th>
                                     <th class="text-center text-uppercase text-dark text-xxs font-weight-bolder" width="11%">Action</th>
                                 </tr>
                             </thead>
-                            <tbody id="equipment_table_body">
+                            <tbody id="late_table_body">
                                 @foreach ($lateTransaction as $transaction)
                                     <tr>
                                         <td style="vertical-align: middle;">{{ $transaction->borrowed_by }}</td>
                                         <td style="vertical-align: middle;">{{ $transaction->office_name }}</td>
+                                        <td style="vertical-align: middle;">{{ $transaction->release_by }}</td>
                                         {{-- <td style="vertical-align: middle;">{{ $transaction->equipment_name }}</td>
                                         <td style="vertical-align: middle;">{{ $transaction->property_no }}</td>
                                         <td style="vertical-align: middle;">{{ $transaction->serial_no }}</td> --}}
@@ -87,7 +145,7 @@
                                         <td style="vertical-align: middle;">
                                             {{ \Carbon\Carbon::parse($transaction->date_returned)->format('F d, Y | h:i A') }}
                                         </td>
-                                        <td style="vertical-align: middle;">{{ $transaction->release_by }}</td>
+                                        
 
 
                                         {{-- <td style="vertical-align: middle;">{{ ucfirst($transaction->status) }}</td> --}}
