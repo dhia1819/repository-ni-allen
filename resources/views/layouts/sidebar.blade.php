ea4ae734e9352583d9e1d6dfbb9593473f7abe2a
dhia1819 <dhiaaguila@gmail.com>
2024-04-18 14:03:20 +0800

    ano

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="#">
+        <a class="navbar-brand m-0" href="/home">
             <img src="/images/archive.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1">{{ config('app.name') }}</span>
         </a>
